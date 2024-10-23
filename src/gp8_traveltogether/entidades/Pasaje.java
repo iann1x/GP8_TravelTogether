122d3f48a4045ab541e23291869814def0dd26aa
iann1x <ianchiti308@gmail.com>
2024-10-22 23:40:56 -0300

    no la guarde xd (+2 clases)

--- src/gp8_traveltogether/entidades/Pasaje.java
+++ src/gp8_traveltogether/entidades/Pasaje.java
@@ -4,10 +4,80 @@
  */
 package gp8_traveltogether.entidades;
 
+import java.time.LocalDateTime;
+
 /**
  *
  * @author Administrator
  */
 public class Pasaje {
+    
+    private int codPasaje;
+    private LocalDateTime fechaHora;
+    private Ciudad origen;
+    private Ciudad destino;
+    private double precioPasaje;
+    private String tipoViaje;
+
+    public Pasaje(int codPasaje, LocalDateTime fechaHora, Ciudad origen, Ciudad destino, double precioPasaje, String tipoViaje) {
+        this.codPasaje = codPasaje;
+        this.fechaHora = fechaHora;
+        this.origen = origen;
+        this.destino = destino;
+        this.precioPasaje = precioPasaje;
+        this.tipoViaje = tipoViaje;
+    }
+
+    public Pasaje() {
+    }
+
+    public int getCodPasaje() {
+        return codPasaje;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    public Ciudad getOrigen() {
+        return origen;
+    }
+
+    public Ciudad getDestino() {
+        return destino;
+    }
+
+    public double getPrecioPasaje() {
+        return precioPasaje;
+    }
+
+    public String getTipoViaje() {
+        return tipoViaje;
+    }
+
+    public void setCodPasaje(int codPasaje) {
+        this.codPasaje = codPasaje;
+    }
+
+    public void setFechaHora(LocalDateTime fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public void setOrigen(Ciudad origen) {
+        this.origen = origen;
+    }
+
+    public void setDestino(Ciudad destino) {
+        this.destino = destino;
+    }
+
+    public void setPrecioPasaje(double precioPasaje) {
+        this.precioPasaje = precioPasaje;
+    }
+
+    public void setTipoViaje(String tipoViaje) {
+        this.tipoViaje = tipoViaje;
+    }
+    
     
 }
--- src/gp8_traveltogether/entidades/Turista.java
+++ src/gp8_traveltogether/entidades/Turista.java
@@ -10,4 +10,51 @@ package gp8_traveltogether.entidades;
  */
 public class Turista {
     
+    private int dni;
+    private String nombre;
+    private int edad;
+    private boolean estado;
+
+    public Turista(int dni, String nombre, int edad, boolean estado) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.estado = estado;
+    }
+
+    public Turista() {
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
 }
