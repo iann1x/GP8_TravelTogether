93199f06b1b722853941b79afa6ffc9249b300d8
facug1559 <facundogarcia1559@gmail.com>
2024-10-22 23:35:15 -0300

    clase alojamiento

---
+++ src/gp8_traveltogether/entidades/Alojamiento.java
@@ -0,0 +1,62 @@
+package gp8_traveltogether.entidades;
+
+import java.util.logging.Logger;
+
+public class Alojamiento {
+    private int codAlojam;
+    private String nombre;
+    private boolean estado;
+    private int capacidad;
+    private double precioNoche;
+
+    public Alojamiento(int codAlojam, String nombre, boolean estado, int capacidad, double precioNoche) {
+        this.codAlojam = codAlojam;
+        this.nombre = nombre;
+        this.estado = estado;
+        this.capacidad = capacidad;
+        this.precioNoche = precioNoche;
+    }
+
+    public int getCodAlojam() {
+        return codAlojam;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public double getPrecioNoche() {
+        return precioNoche;
+    }
+
+    public void setCodAlojam(int codAlojam) {
+        this.codAlojam = codAlojam;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public void setPrecioNoche(double precioNoche) {
+        this.precioNoche = precioNoche;
+    }
+        
+}
+
+    
