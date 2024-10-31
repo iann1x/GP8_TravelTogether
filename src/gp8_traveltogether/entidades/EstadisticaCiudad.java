9a115bc5237c7f771bad8664542ea5290ac11b23
sanchoponcho08 <kevinsito1559@gmail.com>
2024-10-31 19:50:26 -0300

    vista estadisticas sin terminar aun

---
+++ src/gp8_traveltogether/entidades/EstadisticaCiudad.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gp8_traveltogether.entidades;
+
+
+public class EstadisticaCiudad {
+    private int codCiudad;
+    private String nombre;
+    private int frecuencia;
+
+    public int getCodCiudad() {
+        return codCiudad;
+    }
+
+    public void setCodCiudad(int codCiudad) {
+        this.codCiudad = codCiudad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getFrecuencia() {
+        return frecuencia;
+    }
+
+    public void setFrecuencia(int frecuencia) {
+        this.frecuencia = frecuencia;
+    }
+    
+}
